3f0aee425054cb5acc446325071047444b1621d7
zizifn <515868058@qq.com>
2023-02-08 12:41:19 +0800

    init worker

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 export default {
 	fetch() {
+		let result = 'Hello worker!!';
+		if (connect) {
+			result = `${result}  + has connect method`;
+		}
 		return new Response('Hello worker!!', {
 			headers: {
 				'content-type': 'text/plain',
