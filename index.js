a839e0625e13cbdb98e086dde6b927fb8dcdfa1e
zizfn <1803942+zizifn@users.noreply.github.com>
2023-03-25 03:01:29 +0800

    update

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 export default {
-	fetch() {
-		let result = 'Hello worker!!';
-		if (connect) {
-			result = `${result}  + has connect method`;
-		}
-		return new Response(result, {
-			headers: {
-				'content-type': 'text/plain',
-			},
+	async fetch(request) {
+		let count = 0;
+		console.log('test11', Date.now());
+		const transformStream =
+			request.body?.pipeThrough(new TextDecoderStream()).pipeThrough(
+				new TransformStream({
+					transform(chunk, controller) {
+						console.log('test', Date.now());
+						controller.enqueue(new TextEncoder().encode(`${chunk} + ${count++}  ${new Date()}`));
+					},
+				})
+			) || 'default';
+		return new Response(transformStream, {
+			headers: { 'content-type': 'text/plain' },
 		});
 	},
 };
---
+++ nodejs-client-test.mjs
@@ -0,0 +1,39 @@
+import {
+    ReadableStream,
+    TextDecoderStream,
+    TextEncoderStream,
+  } from "node:stream/web";
+  import { fetch } from "undici";
+  
+  try {
+    const readableStream = new ReadableStream({
+      index: 0,
+      start(controller) {
+        const interval = setInterval(() => {
+          controller.enqueue(`client send ${this.index++}`);
+        }, 500);
+  
+        setTimeout(() => {
+          clearInterval(interval);
+          controller.close();
+        }, 10_000);
+      },
+    }).pipeThrough(new TextEncoderStream());
+  
+    const url = "https://wroker-poc.zizifn.workers.dev/";
+  
+    const resp = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "text/plain",
+      },
+      body: readableStream,
+      duplex: "half",
+    });
+  
+    for await (let chunk of resp.body.pipeThrough(new TextDecoderStream())) {
+      console.log(chunk);
+    }
+  } catch (err) {
+    console.log(err);
+  }
